c0c4db80ac8728f33d005d96557c834bacb1bc2c
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-07-08 19:04:56 -0700

    add questions and answers to store in app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Example store structure
  */
@@ -5,24 +7,54 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What Disaster happened in August of 2020?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Jello factory explosion',
+        'Alien invasion',
+        'solar flares',
+        'tiny frogs jam up highway trucking systems'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'tiny frogs jam up highway trucking systems'
     },
     {
-      question: 'What is the current year?',
+      question: 'What Disaster happened in September of 2020?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'aliens steal the pyramids',
+        'killer bees take over france',
+        'world wide coffee shortages',
+        'nation wide paper shortage'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'world wide coffee shortages'
+    },
+    {
+      question: 'What Disaster happened in October of 2020?',
+      answers: [
+        'timmys mom runs out of coffee',
+        'timmys mom yells at timmy',
+        'timmy drops icecream on stonehenge',
+        'all of the above'
+      ],
+      correctAnswer: 'all of the above'
+    },
+    {
+      question: 'What Disaster happened in November of 2020?',
+      answers: [
+        'locusts eat all the cats',
+        'chickens stop laying eggs',
+        "timmy's icecream opens a wormhole in stonehenge",
+        'cows stop producing milk'
+      ],
+      correctAnswer: "timmy's icecream opens a wormhole in stonehenge"
+    },
+    {
+      question: 'What Disaster happened in December of 2020?',
+      answers: [
+        'ramen goes out of business',
+        'Java finally installs',
+        'Jets win the Superbowl early',
+        'capuchin monkeys traverse the wormhole and reveal the illuminati'
+      ],
+      correctAnswer: 'capuchin monkeys traverse the wormhole and reveal the illuminati'
     }
   ],
   quizStarted: false,
