faef8247d27f7c4fc2e6d13f8dd857f6b98046fc
nhemerling <65194792+nhemerling@users.noreply.github.com>
2020-07-12 18:21:33 -0700

    Pull our quiz app.  (#1)
    * change h1 on home wireframe  to name of quiz
    
    * did stuff
    
    * updated quiz name on index.html
    
    * added stuff
    
    * add questions and answers to store in app.js
    
    * capitalized questions and answers appropriately
    
    * added pseudocode for functions in app.js
    
    * add basic templates for the functions in app.js
    
    * incorporate store data in generateQuestionPageTemplate function
    
    * yay
    
    * add classes to buttons
    
    * check out the render quiz function
    
    * m
    
    * updated
    
    * add main function at the bottom of js and attempt to debug render function
    
    * updated
    
    * debug handleStartQuiz()
    
    * added stuff
    
    * handleStartQuiz() debug
    
    * m
    
    * test
    
    * event handler functions comment add
    
    * m
    
    * add main function
    
    * add handle answer submit func
    
    * m
    
    * add handleNextQuestionClicked()
    
    * debug handleAnswerSubmit() to increase score if user got the answer correct
    
    * m
    
    * wrongAnswerPageTemplate() should now offer correct answer
    
    * merged our work
    
    * m
    
    * m
    
    * nicholas crap
    
    * m
    
    * added score to bottom of pages
    
    * m
    
    * retake quiz button should work
    
    * uh oh
    
    * pass the mic
    
    * final image fix and radio button default
    
    * pass the mic
    
    * round and big start button
    
    * push
    
    * test
    
    * fixed spacing with the value of form inputs
    
    * need a fix
    
    * css work
    
    * pass the mic
    
    * center submit button
    
    * edits to the copy and shrink final image to accommodate retake quiz button
    
    * pass it off
    
    * style buttons and adjust fonts
    
    * adjust slight grey tint to text
    
    * operator error
    
    * operator error
    
    * operator error
    
    * fixed mobile up
    
    * fixed mobile up
    
    * css work
    
    * css work
    
    * final changes start
    
    * final css polish, final code check, passing the mic to Mr. Nick.
    
    * always with the padding in the headers!
    
    * always with the padding in the headers!
    
    * always with the padding in the headers!
    
    * tweakers
    
    * tweakers
    
    * add exclamation point to next button and slight edit to copy
    
    * edit README to say we worked as a pair on this project
    
    * final touches
    
    Co-authored-by: JackieBishop <jax.abert@gmail.com>
    Co-authored-by: Jackie Bishop <63313775+JackieBishop@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-<h1>Quiz APP</h1>
+<h1>Quiz APP by Jackie and Nicholas</h1>
 
 <h2>Objective:</h2> <p>To build an interactive quize application from scratch using javascript, jQuery and css.</p>
+
+<p>This project was worked on by both Jackie Abert and Nicholas Hemerling.</p>
--- app.js
+++ app.js
@@ -1,58 +1,247 @@
-/**
- * Example store structure
- */
-const store = {
+'use strict';
+//ANSWER KEY IS FINISHED
+const answerKey = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'What Disaster happened in August of 2020?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Jello factory explosion.',
+        'Alien invasion.',
+        'Solar flares.',
+        'Tiny frogs jammed up highway trucking systems.'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'Tiny frogs jammed up highway trucking systems.'
     },
     {
-      question: 'What is the current year?',
+      question: 'What Disaster happened in September of 2020?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Aliens stole the pyramids.',
+        'Killer bees took over France.',
+        'World-wide coffee shortage.',
+        'Nation wide paper shortage.'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'World-wide coffee shortage.'
+    },
+    {
+      question: 'What Disaster happened in October of 2020?',
+      answers: [
+        'Timmy\'s mom ran out of coffee.',
+        'Timmy\'s mom yelled at timmy.',
+        'Timmy dropped icecream on stonehenge.',
+        'All of the above.'
+      ],
+      correctAnswer: 'All of the above.'
+    },
+    {
+      question: 'What Disaster happened in November of 2020?',
+      answers: [
+        'Locusts ate all the cats.',
+        'Chickens stopped laying eggs.',
+        'Timmy\'s icecream opened a wormhole in stonehenge.',
+        'Cows stopped producing milk.'
+      ],
+      correctAnswer: 'Timmy\'s icecream opened a wormhole in stonehenge.'
+    },
+    {
+      question: 'What Disaster happened in December of 2020?',
+      answers: [
+        'Ramen went out of business.',
+        'Java finally installed.',
+        'Jets win the Superbowl early.',
+        'Capuchin monkeys traversed the wormhole and revealed the illuminati.'
+      ],
+      correctAnswer: 'Capuchin monkeys traversed the wormhole and revealed the illuminati.'
     }
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
+  finalImage: '<img class="image2" src="images/monkey-angry.jpg" alt="Angry-little-monkey.jpg"></img>',
+  endStatement: 'You FAIL! The monkeys are angry.' 
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+//TEMPLATES ARE COMPLETE
+function homePageTemplate() {
+  // this function will return the HTML template for the home page
+  return `<div class="container">
+            <div class="img-container">
+              <img class="image" src="images/monkey-title-image.jpg" alt="Capuchin Monkey sticking its tongue out">
+            </div>
+            <div>
+              <button class="start-quiz">Start Quiz</button>
+            </div>
+          </div>`;
+}
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+function questionPageTemplate() {
+  // this function will return the HTML template for the next question
+  const question = answerKey.questions[answerKey.questionNumber];
+  return `<div class ="container">
+            <div class="quiz-container">
+              <div>
+                <h3 class="question-number">Question number ${[answerKey.questionNumber+1]} of ${answerKey.questions.length}:<h3>
+                <h2>${question.question}</h2>
+              </div>
+              <div>
+                <form id="quiz-form">
+                <label class="flex-label"><input type="radio" name = "ans" value="${question.answers[0]}" checked>${question.answers[0]}</label></br>
+                <label class="flex-label"><input type="radio" name = "ans" value="${question.answers[1]}">${question.answers[1]}</label></br>
+                <label class="flex-label"><input type="radio" name = "ans" value="${question.answers[2]}">${question.answers[2]}</label></br>
+                <label class="flex-label"><input type="radio" name = "ans" value="${question.answers[3]}">${question.answers[3]}</label></br>
+                  <div class="submit-button">
+                    <button type="submit">Submit!</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="score">
+              <h3>Current Score: ${answerKey.score}/${answerKey.questions.length}</h3>
+            </div>
+          </div>`
+};
 
-// These functions return HTML templates
+function correctAnswerPageTemplate() {
+  // this function will return the HTML template for if the user answered
+  // the question correctly
+  return `<div class="container">
+            <div class="quiz-container">
+              <div>
+                <h2>You really know your stuff, eh?</h2>
+                <p>Impressive. Moving right along...</p>
+              </div>
+              <div>
+                <button class="next-question">Next!</button>
+              </div>
+            </div>
+            <div class="score">
+              <h3>Current Score: ${answerKey.score}/${answerKey.questions.length}</h3>
+            </div>
+          </div>`;
+}
 
-/********** RENDER FUNCTION(S) **********/
+function wrongAnswerPageTemplate() {
+  // this function will return the HTML template for if the user answered
+  // the question incorrectly
+  return `<div class="container">
+            <div class="quiz-container">
+              <div>
+                <h2>That's not what happened in the latter half of the year 2020!</h2>
+                <p>The correct answer was: ${answerKey.questions[answerKey.questionNumber].correctAnswer}</p>
+              </div>
+              <div>
+                <button class="next-question">Next!</button>
+              </div>
+            </div>
+            <div class="score">
+              <h3>Current Score: ${answerKey.score}/${answerKey.questions.length}</h3>
+            </div>
+          </div>`;
+}
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+function finalScorePageTemplate() {
+  return `<div class="container final-container">
+            <div class="img-container final-image">
+              ${answerKey.finalImage}
+            </div>
+            <div class="results">
+              <h2>Final Score: ${answerKey.score}/${answerKey.questions.length}</h2>
+              <h3>${answerKey.endStatement}</h3>
+            </div>
+            <div>
+              <button class="retake-quiz">Retake Quiz</button>
+            </div>
+          </div>`;
+}
+
+//********** RENDER FUNCTION(S) **********//
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store//
+
+
+function loadQuizPage() {
+  // accepts the HTML template and inserts it into the DOM
+  $("main").html(homePageTemplate);
+  console.log('`loadQuizPage` ran');
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
-// These functions handle events (submit, click, etc)
+function beforeStartQuiz() {
+  console.log('`beforeStartClicked` ran');
+  $('.container').on('click', '.start-quiz', event => {
+    answerKey.quizStarted = true;
+    console.log(answerKey.quizStarted);
+    $("main").html(questionPageTemplate);
+  });
+}
+
+function beforeAnswerSubmit() {
+  // this function will listen for when the start button is clicked
+  console.log('`beforeAnswerSubmit` ran');
+  $('main').on("submit", function(event) {
+    event.preventDefault();
+    // find if the user got the question right or wrong    
+    const answer = answerKey.questions[answerKey.questionNumber].correctAnswer;
+    const selectedAnswer = $('input[name="ans"]:checked').val(); 
+    console.log(answer);
+    console.log(selectedAnswer);
+    let outputFunction = correctAnswerPageTemplate
+    if (selectedAnswer === answer){
+      answerKey.score ++;
+      console.log("correct answer")
+    }else{
+      outputFunction = wrongAnswerPageTemplate
+      console.log('wrong answer');
+    } 
+    $('main').html(outputFunction());
+  });
+}
+
+function beforeNextQuestionCLicked() {
+  // this function will listen for when the next question button is clicked
+  console.log('`beforeNextQuestionClicked` ran');
+  $('main').on('click', '.next-question', event => {
+    // update question template with next question
+    answerKey.questionNumber ++;
+    // render it to the DOM
+    if (answerKey.questionNumber < 5) {
+      $("main").html(questionPageTemplate);
+    } else {
+      finalImageSwap();
+      $("main").html(finalScorePageTemplate);
+    }
+  });
+  console.log("afterNextQuestionCLicked working");
+}
+
+function beforeRetakeQuizClicked() {
+  // this function will listen for when the retake quiz button is clicked
+  $('main').on('click', '.retake-quiz', event => {
+    location.reload(true);
+    // render it to the DOM
+    console.log('`retakeQuizClicked` ran');
+    renderQuizPage();
+  });
+  console.log('`beforeRetakeQuizClicked` ran');
+}
+
+function finalImageSwap() {
+  if (answerKey.score === 5) {
+    console.log("i work!");
+    answerKey.finalImage = '<img class="image2" src="images/monkey-happy.jpg" alt="Happy-little-monkey.jpg"></img>'
+    answerKey.endStatement = 'You WIN! The monkeys are happy.' 
+  }
+}
+ 
+function onDocumentReady() {
+  loadQuizPage();
+  beforeStartQuiz();
+  beforeAnswerSubmit();
+  beforeNextQuestionCLicked();
+  beforeRetakeQuizClicked();
+  finalImageSwap();
+  beforeRetakeQuizClicked();
+};
+
+$(onDocumentReady);
